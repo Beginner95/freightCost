0237d51bc2574e99309d07256b5f00b0acf34138
Vaharsolta <vaha-22@mail.ru>
2019-12-26 22:26:54 +0300

    Added migration for table weights

---
+++ database/migrations/2019_12_26_192030_create_weights_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWeightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weights', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 200);
+            $table->string('cubic_meter', 200);
+            $table->double('price', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weights');
+    }
+}
