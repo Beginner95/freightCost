38400c1c84a4e5e7334e294879502ea34dfe9155
Vaharsolta <vaha-22@mail.ru>
2019-12-27 10:20:54 +0300

    Registration blocked

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,11 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'IndexController@index')->name('index');
+
+Route::get('/register', function () {
+   return redirect('/admin/index');
+});
 
 Route::group(['namespace' => 'Auth'], function() {
     Route::get('login', 'LoginController@showLoginForm')->name('login');
