703928f2ace53574c4f2e7d5d0c0226fff6e206c
Vaharsolta <vaha-22@mail.ru>
2020-03-05 00:29:52 +0300

    Added redirect method to login page

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $weights = Weight::get();
