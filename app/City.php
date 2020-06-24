ff6a776c5197398a8bfae034e7def484ee6f9f71
Vaharsolta <vaha-22@mail.ru>
2020-06-24 13:59:54 +0300

    Added properties

--- app/City.php
+++ app/City.php
@@ -4,6 +4,11 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string name
+ * @property integer region_id
+ * @property string location
+ */
 class City extends Model
 {
     protected $fillable = ['region_id', 'name', 'location'];
