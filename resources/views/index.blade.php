7cd25a597d628289187f09aae57dc57d3a465fbe
Vaharsolta <vaha-22@mail.ru>
2020-07-01 21:33:21 +0300

    Edit basic template

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,20 +1,37 @@
 @extends('layouts.app')
 
 @section('content')
-    <div style="display: none">
-        <input id="origin-input" class="controls" type="text" placeholder="Город погрузки">
-        <input id="destination-input" class="controls" type="text" placeholder="Город выгрузки">
-    </div>
-    <div class="clear-input" id="clear-input-a"></div>
-    <div class="clear-input" id="clear-input-b"></div>
-    <a href="{{ route('logout') }}" class="logout-icon"></a>
-    <div class="weight">
-        <ul class="panel_list d-flex block-weight"></ul>
-        <div class="block-inform">
-            <p>Растояние: <span class="text-distance"></span></p>
-            <p>Цена перевозки: <span class="text-price"></span></p>
+<div class="container-site open-sidebar" style="background: #4c783f;">
+    <div class="main-content">
+        <div class="content">
+            <div class="roundbox-position">
+                <div class="roundbox roundbox-width1 roundbox-p3">
+                    <div id="panel-from-in" class="controls panel">
+                        <input id="address" type="text" placeholder="Город погрузки" class="SearchBox">
+                    </div>
+                    <div id="panel2-from-in" class="controls panel">
+                        <input id="address2" type="text" placeholder="Город выгрузки" class="SearchBox">
+                    </div>
+                    <div id="panelClose1" class="panelClose" onClick="address.click();"></div>
+                    <div id="panelClose2" class="panelClose" onClick="address2.click();"></div>
+                </div>
+                <input type="hidden" name="svidTransp" id="svidTransp" value="2">
+            </div>
+            <div id="map-canvas"></div>
         </div>
     </div>
+</div>
 
-    <div id="map"></div>
+<div class="b-popup" id="popup1">
+    <img src="/img/loading.gif">
+</div>
+
+<a href="{{ route('logout') }}" class="logout-icon"></a>
+<div class="weight">
+    <ul class="panel_list d-flex block-weight"></ul>
+    <div class="block-inform">
+        <p class="block-inform-p">Растояние: <span class="text-distance"></span></p>
+        <p class="block-inform-p">Цена перевозки: <span class="text-price"></span></p>
+    </div>
+</div>
 @endsection
