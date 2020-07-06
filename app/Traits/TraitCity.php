011dd413adea2c6704e6c738eaf869eed2c9a963
Vaharsolta <vaha-22@mail.ru>
2020-07-06 12:40:58 +0300

    Removed checking variable for array

--- app/Traits/TraitCity.php
+++ app/Traits/TraitCity.php
@@ -8,30 +8,26 @@ trait TraitCity
     use Location;
     public function getCityId($city)
     {
-        if (is_array($city)) {
-            $cityName = explode(',', $city)[0];
-        } else {
-            $cityName = $city;
-        }
+        $cityName = explode(',', $city)[0];
 
         $city = City::select('id', 'location')
             ->where('name', $cityName)
             ->first();
+
+        if (empty($city)) {
+            $city = new City();
+            $city->name = $cityName;
+            $city->region_id = 0;
+            $city->location = $this->getLocation($cityName);
+            $city->save();
+            return $city->id;
+        }
 
         if ($city->location === null) {
             $location = $this->getLocation($cityName);
             City::where('name', $cityName)->update(['location' => $location]);
         }
 
-        if (empty($city)) {
-            $city = new City();
-            $city->name = $cityName;
-            $city->region_id = 0;
-            $location = $this->getLocation($cityName);
-            if ($this->isJson($location)) $city->location = $location;
-            $city->save();
-            return $city->id;
-        }
         return $city->id;
     }
 }
